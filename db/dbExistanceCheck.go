79004ac72aa84db3e9c96eefba6339aa6fa76481
frit2000 <frit2000@yandex.ru>
2024-04-27 10:28:15 +0300

    updated steps 1-2

--- db/dbExistanceCheck.go
+++ db/dbExistanceCheck.go
@@ -15,32 +15,33 @@ func DbExistance() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	envFile := os.Getenv("TODO_DBFILE")
-	fmt.Println("env=", envFile)
-	if len(envFile) > 0 {
-		appPath = envFile
-	}
+	// envFile := os.Getenv("TODO_DBFILE")
+	// if len(envFile) > 0 {
+	// 	fmt.Println("зашли в env")
+	// 	appPath = envFile
+	// }
 
 	dbFile := filepath.Join(filepath.Dir(appPath), "scheduler.db")
 	_, err = os.Stat(dbFile)
 
-	//	var install bool
 	if err != nil {
 		dbCreate()
 	}
-	// если install равен true, после открытия БД требуется выполнить
-	// sql-запрос с CREATE TABLE и CREATE INDEX
 }
 
 func dbCreate() {
-	db, err := sql.Open("sqlite", "db/scheduler.db")
+	db, err := sql.Open("sqlite", "scheduler.db")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer db.Close()
 
-	_, err = db.Exec("CREATE TABLE scheduler (id INTEGER PRIMARY KEY AUTOINCREMENT,date CHAR(8),title VARCHAR(128),comment VARCHAR(256),repeat VARCHAR(128))")
+	_, err = db.Exec(`CREATE TABLE scheduler (id INTEGER PRIMARY KEY AUTOINCREMENT,	
+											date CHAR(8) NOT NULL DEFAULT "", 
+											title VARCHAR(128) NOT NULL DEFAULT "", 
+											comment VARCHAR(256) NOT NULL DEFAULT "",
+											repeat VARCHAR(128))`)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -53,6 +54,5 @@ func dbCreate() {
 	}
 
 	row := db.QueryRow("SELECT * FROM scheduler")
-	fmt.Println("base result=", row)
 
 }
