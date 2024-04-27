23431dda283c4d635b7ac7b892bbd32378148f2b
frit2000 <frit2000@yandex.ru>
2024-04-27 16:56:48 +0300

    3rd step with weekdays , tests passed

--- nextdate/nextdate.go
+++ nextdate/nextdate.go
@@ -17,18 +17,13 @@ func NextDate(now time.Time, date string, repeat string) (string, error) {
 		return "", fmt.Errorf("ошибка формата repeat, пустая строка")
 	}
 
-	// if date < now.Format("20060102") {
-	// 	date = now.Format("20060102")
-	// }
-
-	// if repeat == nil {
-	// 	return now.Format("20060102"), nil
-	// }
 	switch repeat[0] {
 	case 'd':
 		return addDays(now, dateInTimeFormat, repeat)
 	case 'y':
 		return addYear(now, dateInTimeFormat)
+	case 'w':
+		return addWeek(now, dateInTimeFormat, repeat)
 	default:
 		return "", fmt.Errorf("ошибка формата repeat, первый символ не допустим")
 	}
@@ -65,4 +60,48 @@ func addYear(now time.Time, dateInTimeFormat time.Time) (string, error) {
 		dateInTimeFormat = dateInTimeFormat.AddDate(1, 0, 0)
 	}
 	return dateInTimeFormat.Format("20060102"), nil
+}
+
+func addWeek(now time.Time, dateInTimeFormat time.Time, repeat string) (string, error) {
+	daySlice := strings.Split(repeat, " ")
+	if len(daySlice) != 2 {
+		return "", fmt.Errorf("ошибка формата repeat, дни недели не заданы")
+	}
+	weekDaySlice := strings.Split(daySlice[1], ",")
+	//делаем мапу доспустимых значений дней, на которые можно перенести задачу
+	validDays := make(map[int]string)
+	for _, day := range weekDaySlice {
+		dayInt, err := strconv.Atoi(day)
+		if err != nil {
+			return "", fmt.Errorf("ошибка формата repeat, день недели не число")
+		}
+		if dayInt > 7 || dayInt < 1 {
+			return "", fmt.Errorf("ошибка формата repeat, день недели не от 1 до 7")
+		}
+		validDays[dayInt] = day
+	}
+
+	//проверяем, что считаем от даты которая дальше чем сегодня, иначе считаем с сегодня
+	if dateInTimeFormat.Format("20060102") < now.Format("20060102") {
+		dateInTimeFormat = now
+	}
+
+	//если в мапе есть допустимый ключ для переноса дня, переносим на этот день
+	for i := 1; i <= 7; i++ {
+		dateInTimeFormat = dateInTimeFormat.AddDate(0, 0, 1)
+		weekDay := weekDay(dateInTimeFormat)
+		_, ok := validDays[weekDay]
+		if ok {
+			return dateInTimeFormat.Format("20060102"), nil
+		}
+	}
+	return "", fmt.Errorf("что-то глобально пошло не так в переносе на день недели")
+}
+
+func weekDay(day time.Time) int {
+	weekDay := day.Weekday()
+	if int(weekDay) == 0 {
+		return 7
+	}
+	return int(weekDay)
 }
